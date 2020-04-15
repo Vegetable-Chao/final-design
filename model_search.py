2af98776eaf514351f2d2bf3642337ffaae28b78
66430 <66430@local>
2020-04-15 21:48:15 +0800

    preparing to bring in searched reduction cell

--- model_search.py
+++ model_search.py
@@ -149,19 +149,22 @@ class Ops(nn.Module):
         self.mp = nn.MaxPool2d(2, 2)
         
     def forward(self,x):
-        dim_2 = x.shape[1]
-        xtemp = x[:, :  dim_2//self.K, :, :]
-        xtemp2 = x[:,  dim_2//self.K:, :, :]
-        temp1 = self.op[self.number](xtemp)
-        # reduction cell needs pooling before concat                           ###***
-        if temp1.shape[2] == x.shape[2]:
-            ans = torch.cat([temp1, xtemp2], dim=1)
+        if self.K == 1:
+            return self.op[self.number](x)
         else:
-            ans = torch.cat([temp1, self.mp(xtemp2)], dim=1)  # ？？？
-        ans = channel_shuffle(ans, self.K)
-        #ans = torch.cat([ans[ : ,  dim_2//4:, :, :],ans[ : , :  dim_2//4, :, :]],dim=1)
-        # except channe shuffle, channel shift also works
-        return ans
+            dim_2 = x.shape[1]
+            xtemp = x[:, :  dim_2//self.K, :, :]
+            xtemp2 = x[:,  dim_2//self.K:, :, :]
+            temp1 = self.op[self.number](xtemp)
+            # reduction cell needs pooling before concat                           ###***
+            if temp1.shape[2] == x.shape[2]:
+                ans = torch.cat([temp1, xtemp2], dim=1)
+            else:
+                ans = torch.cat([temp1, self.mp(xtemp2)], dim=1)  # ？？？
+            ans = channel_shuffle(ans, self.K)
+            #ans = torch.cat([ans[ : ,  dim_2//4:, :, :],ans[ : , :  dim_2//4, :, :]],dim=1)
+            # except channe shuffle, channel shift also works
+            return ans
         
             
         
@@ -338,95 +341,5 @@ class Network(nn.Module):
     
     def arch_parameters(self):
         return self._arch_parameters
-
-
-# class Network(nn.Module):
-
-#     def __init__(self, C, num_classes, layers, criterion, steps=4, multiplier=4, stem_multiplier=3, switches_normal=[], switches_reduce=[], p=0.0):
-#         super(Network, self).__init__()
-#         self._C = C
-#         self._num_classes = num_classes
-#         self._layers = layers
-#         self._criterion = criterion
-#         self._steps = steps
-#         self._multiplier = multiplier
-#         self.p = p
-#         self.switches_normal = switches_normal
-#         switch_ons = []
-#         for i in range(len(switches_normal)):
-#             ons = 0
-#             for j in range(len(switches_normal[i])):
-#                 if switches_normal[i][j]:
-#                     ons = ons + 1
-#             switch_ons.append(ons)
-#             ons = 0
-#         self.switch_on = switch_ons[0]
-
-#         C_curr = stem_multiplier*C
-#         self.stem = nn.Sequential(
-#             nn.Conv2d(3, C_curr, 3, padding=1, bias=False),
-#             nn.BatchNorm2d(C_curr)
-#         )
-    
-#         C_prev_prev, C_prev, C_curr = C_curr, C_curr, C
-#         self.cells = nn.ModuleList()
-#         reduction_prev = False
-#         for i in range(layers):
-#             if i in [layers//3, 2*layers//3]:
-#                 C_curr *= 2
-#                 reduction = True
-#                 cell = Cell(steps, multiplier, C_prev_prev, C_prev, C_curr, reduction, reduction_prev, switches_reduce, self.p)
-#             else:
-#                 reduction = False
-#                 cell = Cell(steps, multiplier, C_prev_prev, C_prev, C_curr, reduction, reduction_prev, switches_normal, self.p)
-# #            cell = Cell(steps, multiplier, C_prev_prev, C_prev, C_curr, reduction, reduction_prev, switches)
-#             reduction_prev = reduction
-#             self.cells += [cell]
-#             C_prev_prev, C_prev = C_prev, multiplier*C_curr
-
-#         self.global_pooling = nn.AdaptiveAvgPool2d(1)
-#         self.classifier = nn.Linear(C_prev, num_classes)
-
-#         self._initialize_alphas()
-
-#     def forward(self, input):
-#         s0 = s1 = self.stem(input)
-#         for i, cell in enumerate(self.cells):
-#             if cell.reduction:
-#                 if self.alphas_reduce.size(1) == 1:
-#                     weights = F.softmax(self.alphas_reduce, dim=0)
-#                 else:
-#                     weights = F.softmax(self.alphas_reduce, dim=-1)
-#             else:
-#                 if self.alphas_normal.size(1) == 1:
-#                     weights = F.softmax(self.alphas_normal, dim=0)
-#                 else:
-#                     weights = F.softmax(self.alphas_normal, dim=-1)
-#             s0, s1 = s1, cell(s0, s1, weights)
-#         out = self.global_pooling(s1)
-#         logits = self.classifier(out.view(out.size(0),-1))
-#         return logits
-
-#     def update_p(self):
-#         for cell in self.cells:
-#             cell.p = self.p
-#             cell.update_p()
-    
-#     def _loss(self, input, target):
-#         logits = self(input)
-#         return self._criterion(logits, target) 
-
-#     def _initialize_alphas(self):
-#         k = sum(1 for i in range(self._steps) for n in range(2+i))
-#         num_ops = self.switch_on
-#         self.alphas_normal = nn.Parameter(torch.FloatTensor(1e-3*np.random.randn(k, num_ops)))
-#         self.alphas_reduce = nn.Parameter(torch.FloatTensor(1e-3*np.random.randn(k, num_ops)))
-#         self._arch_parameters = [
-#             self.alphas_normal,
-#             self.alphas_reduce,
-#         ]
-    
-#     def arch_parameters(self):
-#         return self._arch_parameters
 
 
--- train_search.py
+++ train_search.py
@@ -20,7 +20,7 @@ from genotypes import Genotype
 
 parser = argparse.ArgumentParser("cifar")
 parser.add_argument('--workers', type=int, default=2, help='number of workers to load dataset')
-parser.add_argument('--batch_size', type=int, default=128, help='batch size')
+parser.add_argument('--batch_size', type=int, default=256, help='batch size')
 parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
 parser.add_argument('--learning_rate_min', type=float, default=0.0, help='min learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
